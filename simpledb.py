2dd197218b3037d1918b6eaf5df047103622381e
llCherubimll <llcherubimll@llCherubimlls-MacBook-Pro.local>
2020-04-16 09:56:50 -0700

    fixed formating

--- simpledb.py
+++ simpledb.py
@@ -7,11 +7,17 @@ from io import BytesIO
 from socket import error as socket_error
 
 
-#exceptions to notify the connection-haneling loop problems.
-class CommandError(Exception): pass
-class Disconnected(Exception): pass
+# exceptions to notify the connection-haneling loop problems.
+class CommandError(Exception):
+    pass
+
+
+class Disconnected(Exception):
+    pass
+
 
 Error = namedtuple('Error', ('message',))
+
 
 class ProtocolHandler(object):
     def handle_request(self, socket_file):
@@ -25,27 +31,27 @@ class ProtocolHandler(object):
                 '*': self.handle_array,
                 '%': self.handle_dict
             }
-        
+       
         def handle_request(self, socket_file):
             first_byte = socket_file.read(1)
             if not first_byte:
                 raise Disconnect()
-            
+           
             try:
                 # Delegate to the appropriate handler based on the first byte.
                 return self.handlers[first_byte](socket_file)
             except KeyError:
                 raise CommandError('bad request')
-        
+      
         def handle_simple_string(self, socket_file):
             return socket_file.readline().rstrip('\r\n')
-        
+      
         def handle_error(self, socket_file):
             return Error(socket_file.readline().rstrip('\r\n'))
-        
+   
         def handle_integer(self, socket_file):
-            return int(socket_file.readline().rstrip('\r\n'))
-        
+            return int(socket_file.readline().rstrip('\r\n')      
+       
         def handle_string(self, socket_file):
             # First read the length ($<length>\r\n).
             length = int(socket_file.readline().rstrip('\r\n'))
@@ -53,11 +59,11 @@ class ProtocolHandler(object):
                 return None # Special case for NULLs.
             length += 2 # Include the trailing \r\n in count.
             return socket_file.read(length)[:-2]
-        
+      
         def handle_array(self, socket_file):
             num_elements = int(socket_file.readline().rstrip('\r\n'))
             return [self.handle_request(socket_file) for _ in range(num_elements)]
-        
+    
         def handle_dict(self, socket_file):
             num_items = int(socket_file.readline().rstrip('\r\n'))
             elements = [self.handle_request(socket_file) for _ in range(num_items * 2)]
@@ -84,4 +90,3 @@ class Server(object):
 
     def run(self):
         self._server.serve_forever()
-
